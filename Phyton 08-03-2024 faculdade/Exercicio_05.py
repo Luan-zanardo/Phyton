6b5d1dec8ec028713ea7f93858e50293c42fab2a
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-03-20 11:30:51 -0300

    Add files via upload

---
+++ Phyton 08-03-2024 faculdade/Exercicio_05.py
@@ -0,0 +1,6 @@
+print("Converter farenheit p/ celsius")
+farenheit = int(input("Digite graus farenheit: "))
+
+celsius = (farenheit - 32) * 5/9
+print(f"{farenheit} farenheit é igual a {celsius} celsius ")
+
