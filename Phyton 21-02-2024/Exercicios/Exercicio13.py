2bba76461e77b00fccf6c5c50239e9f2f0ec1cdf
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-04-02 16:53:43 -0300

    Delete Phyton 21-02-2024 directory

--- Phyton 21-02-2024/Exercicios/Exercicio13.py
+++
@@ -1,9 +0,0 @@
-'''13) Ler 3 valores (considere que não serão informados valores iguais) e escrever a soma dos 2 maiores.'''
-
-valor1 = int(input("Digite valor 1: "))
-valor2 = int(input("Digite valor 2: "))
-valor3 = int(input("Digite valor 3: "))
-
-valoresordenados = sorted([valor1, valor2, valor3], reverse=True)
-maiores = valoresordenados[0] + valoresordenados[1]
-print(f"soma dos maiores valores: {maiores}")
--- Phyton 21-02-2024/Exercicios/Exercicio19.py
+++
@@ -1,6 +0,0 @@
-'''19) Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.'''
-
-numeros = [10,9,8,7,6,5,3,1,2,4]
-
-numeros.sort()
-print(numeros)
