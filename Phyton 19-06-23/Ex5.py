560212e6434f3e3f7ca24588a25bb8a50f088250
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-04-02 16:53:31 -0300

    Delete Phyton 19-06-23 directory

--- Phyton 19-06-23/Ex5.py
+++
@@ -1,8 +0,0 @@
-frase = input("Digite uma frase: ")
-letra = input("Digite uma letra: ")
-
-total_caracteres = len(frase)
-total_letras = frase.count(letra)
-
-print(f"Total de Letras encontradas na frase: {total_letras}")
-print(f"Total de Caracteres na Frase: {total_caracteres}")
