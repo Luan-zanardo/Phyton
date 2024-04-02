bb4daab4be511eb353376ea82e749ac57e7e1155
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-04-02 16:52:48 -0300

    Delete Phyton 05-09-23 directory

--- Phyton 05-09-23/Exercicio3.py
+++
@@ -1,7 +0,0 @@
-"""Faça um script que leia duas notas de um aluno e mostre como resultado sua média."""
-
-a = int(input("Nota 1: "))
-b = int(input("Nota 2: "))
-
-soma = (a + b) / 2
-print(f"A média do aluno é {soma}")
--- Phyton 05-09-23/Exercicio5.py
+++
@@ -1,9 +0,0 @@
-"""Faça um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros."""
-
-metros = int(input("Digite um valor em metros: "))
-
-centímetros = metros * 100
-milímetros = metros * 1000
-
-print(f"{metros} metros equivalem a {centímetros} centímetros")
-print(f"{metros} metros equivalem a {milímetros} milímetros")
