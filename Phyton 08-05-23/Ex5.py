fa6af5f91b986e49a4229bbf8c95b301a775a4f8
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-04-02 16:53:08 -0300

    Delete Phyton 08-05-23 directory

--- Phyton 08-05-23/Ex5.py
+++
@@ -1,4 +0,0 @@
-numero = int(input("Digite um numero: "))
-for contador in range(10):
-    total = numero * (contador + 1) #calcula a multiplicação
-    print("Tabuada = ", numero, "x", contador + 1, " = ", total)
