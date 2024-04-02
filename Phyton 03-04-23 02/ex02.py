36c7f76afb469755fb7a53ba420d104bfc885b2b
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-04-02 16:52:33 -0300

    Delete Phyton 03-04-23 02 directory

--- Phyton 03-04-23 02/ex02.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Digite um valor para a: "))
-b = int(input("Digite um valor para b: "))
-c = int(input("Digite um valor para c: "))
-
-if a + b < c:
-    print(f" a soma de a + b({a + b}) é menor que a soma de c({c})")
-else:
-    print(f" a soma de a + b({a + b}) é maior que a soma de c({c})")
