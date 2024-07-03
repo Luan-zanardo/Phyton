e16331ef83b652698065e41043ae168ad4459217
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-07-02 21:06:52 -0300

    Delete Phyton 23-06-05 directory

--- Phyton 23-06-05/Exercicio03.py
+++
@@ -1,6 +0,0 @@
-vetor = []
-
-for i in range (0,10,1):
-    vetor.append(i)
-
-print(vetor)
--- Phyton 23-06-05/Exercicio08.py
+++
@@ -1,6 +0,0 @@
-vetor=[""]*6
-
-print("Digite 7 números inteiros:")
-for i in range (6):   
-    vetor[i] = input ()
-print (vetor)
