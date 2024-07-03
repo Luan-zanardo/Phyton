c20df149126eedc0d40d75aa9ff2243f6927ae59
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-07-02 21:06:26 -0300

    Delete Phyton 24-02-21 directory

--- Phyton 24-02-21/Exercicios/Exercicio12.py
+++
@@ -1,8 +0,0 @@
-'''12) Ler 3 valores (considere que não serão informados valores iguais) e escrevê-los em ordem crescente.'''
-
-valor1 = int(input("Digite valor 1: "))
-valor2 = int(input("Digite valor 2: "))
-valor3 = int(input("Digite valor 3: "))
-
-valoresordenados = sorted([valor1, valor2, valor3])
-print(valoresordenados)
