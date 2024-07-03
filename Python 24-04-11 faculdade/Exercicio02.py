80f70bf93c3a971bf0b799525789f3535952341b
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-07-02 21:05:59 -0300

    Delete Python 24-04-11 faculdade directory

--- Python 24-04-11 faculdade/Exercicio02.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("Digite numero: "))
-
-if(numero > 0):
-    print(f'{numero} é um numero positivo')
-elif(numero < 0):
-    print(f'{numero} é um numero negativo')
-else:
-    print(f'{numero} é um numero neutro')
--- Python 24-04-11 faculdade/Exercicio04.py
+++
@@ -1,6 +0,0 @@
-letra = input("Digite uma letra: ").upper()
-
-if letra in 'AEIOU':
-    print(f"{letra} é uma vogal")
-else:
-     print(f"{letra} é uma consoante")
