c103248a69ca6b9b25e29d01e42d425145d91da5
Luan Zuchinali Zanardo <97967908+Luan-zanardo@users.noreply.github.com>
2024-07-02 21:07:12 -0300

    Delete Phyton 23-05-08 directory

--- Phyton 23-05-08/Ex2.py
+++
@@ -1,8 +0,0 @@
-nome = input("Digite usuário: ")
-senha = input("Digite senha: ")
-
-while (nome == senha): #repitir quando for igual
-    print("Erro: senha invalida, deve ser diferente do usuário")
-    senha = input("Digite uma nova senha: ")
-
-print("Usuário e senha corretos!!!")
